636c3179ffa96cd669822a5808f68381d95a1ed7
unknown <vrnkmr119@gmail.com>
2022-09-26 18:38:19 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div className="App">
+      <ExpenseItem/>
+      <ExpenseItem/>
+    </div>
+  );
+}
+
+export default App;
